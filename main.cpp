bbe972d1332f114e4e004582545cba65f598933a
Andrew Witten <andrew@motherduck.com>
2024-12-23 14:08:45 -0500

    reads header correctly

--- main.cpp
+++ main.cpp
@@ -6,14 +6,21 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include "duckdb.hpp"
+#include "duckdb/common/enum_util.hpp"
+#include "duckdb/common/enums/wal_type.hpp"
+#include "duckdb/common/serializer/serialization_traits.hpp"
 #include "duckdb/storage/write_ahead_log.hpp"
+#include "duckdb/common/serializer/encoding_util.hpp"
 #include <fcntl.h>
+#include <type_traits>
 
 #define ASSERT(ret) \
     if (!(ret)) {           \
         std::cerr << "failed assertion " << #ret << std::endl; \
         std::abort();        \
     }
+
+using WalUnderlyingType = typename std::underlying_type<duckdb::WALType>::type;
 
 template<typename Buffer>
 struct WalBuffer {
@@ -26,32 +33,63 @@ auto OpenWal(const std::string& wal_path) {
     stat(wal_path.c_str(), &st);
     int fd = open(wal_path.c_str(), O_RDONLY);
     ASSERT(fd > 0);
-    char* buf = (char*)mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    uint8_t* buf = (uint8_t*)mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
     ASSERT(buf != MAP_FAILED);
-    auto rel = [st](char* buf) {
+    auto rel = [st](uint8_t* buf) {
         munmap(buf, st.st_size);
     };
 
-    std::unique_ptr<char, decltype(rel)> b(buf, rel);
-
+    std::unique_ptr<uint8_t, decltype(rel)> b(buf, rel);
     madvise(buf, st.st_size, MADV_SEQUENTIAL);
+    
     WalBuffer<decltype(b)> ret{std::move(b), 
         static_cast<uint64_t>(st.st_size)};
+
     return ret;
 }
 
-void ScanWal(char* buf, uint64_t sz) {
-    char* end = buf + sz;
+void ReadVersion(uint8_t** buf) {
+    duckdb::field_id_t field;
+    field = **buf;
+    // https://github.com/duckdb/duckdb/blob/v1.1.3/src/storage/write_ahead_log.cpp#L164
+    ASSERT(field == 100);
+    (*buf)+=sizeof(field);
+
+    WalUnderlyingType wal_type;
+    uint64_t num_read = duckdb::EncodingUtil::DecodeUnsignedLEB128(*buf, wal_type);
+    ASSERT(wal_type == static_cast<WalUnderlyingType>(duckdb::WALType::WAL_VERSION));
+    (*buf)+=num_read;
+
+    field = **buf;
+    ASSERT(field == 101);
+    (*buf)+=sizeof(field);
+
+    uint64_t wal_version_number;
+    num_read = duckdb::EncodingUtil::DecodeUnsignedLEB128(*buf, wal_version_number);
+    (*buf)+=num_read;
+    ASSERT(wal_version_number == 2);
+
+    field = **buf;
+    ASSERT(field == 0xFF);
+    (*buf)+=sizeof(field);
+
+}
+
+void ScanWal(uint8_t* buf, uint64_t sz) {
+    uint8_t* end = buf + sz;
+    uint8_t* start = buf;
+    ReadVersion(&buf);
+    ASSERT(buf - start == 8);
+
     while(buf < end) {
         uint64_t log_record_size = *(uint64_t*)buf;
         buf += 8;
         uint64_t log_record_checksum = *(uint64_t*)buf;
         buf += 8;
-        //ASSERT((end - buf) > log_record_size);
-        std::cout << (end - buf) << std::endl;
-        std::cout << log_record_size << std::endl;
+        ASSERT((end - buf) >= log_record_size);
         buf += log_record_size;
     }
+    ASSERT(buf == end);
 }
 
 int main(int argc, char** argv) {

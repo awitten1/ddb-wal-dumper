42f8c1ab1e13f62408aee82d7c6e540be46c33e5
Andrew Witten <andrew@motherduck.com>
2024-12-24 16:35:14 -0500

    use ddb api

--- main.cpp
+++ main.cpp
@@ -1,13 +1,20 @@
 
 #include <_types/_uint8_t.h>
 #include <cassert>
+#include <cstddef>
 #include <cstdint>
+#include <cstring>
 #include <iostream>
 #include <memory>
+#include <stdexcept>
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include "duckdb.hpp"
 #include "duckdb/common/checksum.hpp"
+#include "duckdb/common/constants.hpp"
+#include "duckdb/common/serializer/read_stream.hpp"
+#include "duckdb/common/serializer/binary_deserializer.hpp"
+
 #include "duckdb/common/enum_util.hpp"
 #include "duckdb/common/enums/wal_type.hpp"
 #include "duckdb/common/serializer/serialization_traits.hpp"
@@ -23,14 +30,6 @@
         std::abort();        \
     }
 
-using WalUnderlyingType = typename std::underlying_type<duckdb::WALType>::type;
-
-template<typename Buffer>
-struct WalBuffer {
-    Buffer buf;
-    uint64_t size;
-};
-
 auto OpenWal(const std::string& wal_path) {
     struct stat st;
     stat(wal_path.c_str(), &st);
@@ -45,64 +44,167 @@ auto OpenWal(const std::string& wal_path) {
     std::unique_ptr<uint8_t, decltype(rel)> b(buf, rel);
     madvise(buf, st.st_size, MADV_SEQUENTIAL);
 
-    WalBuffer<decltype(b)> ret{std::move(b), 
-        static_cast<uint64_t>(st.st_size)};
-
-    return ret;
+    return std::make_pair(std::move(b), st.st_size);
 }
 
-void ReadVersion(uint8_t** buf) {
-    duckdb::field_id_t field;
-    field = *(duckdb::field_id_t*)(*buf);
+void ReadVersion(duckdb::BinaryDeserializer& deserializer) {
+    deserializer.Begin();
     // https://github.com/duckdb/duckdb/blob/v1.1.3/src/storage/write_ahead_log.cpp#L164
-    ASSERT(field == 100);
-    (*buf)+=sizeof(field);
+    auto wal_type = deserializer.ReadProperty<duckdb::WALType>(100, "wal_type");
+    ASSERT(wal_type == duckdb::WALType::WAL_VERSION);
 
-    WalUnderlyingType wal_type;
-    uint64_t num_read = duckdb::EncodingUtil::DecodeUnsignedLEB128(*buf, wal_type);
-    ASSERT(wal_type == static_cast<WalUnderlyingType>(duckdb::WALType::WAL_VERSION));
-    (*buf)+=num_read;
-
-    field = *(duckdb::field_id_t*)*buf;
-    ASSERT(field == 101);
-    (*buf)+=sizeof(field);
-
-    duckdb::idx_t wal_version_number;
-    num_read = duckdb::EncodingUtil::DecodeUnsignedLEB128(*buf, wal_version_number);
-    (*buf) += num_read;
+    auto wal_version_number = deserializer.ReadProperty<duckdb::idx_t>(101, "version");
     ASSERT(wal_version_number == 2);
-
-    field = *(duckdb::field_id_t*)*buf;
-    ASSERT(field == duckdb::MESSAGE_TERMINATOR_FIELD_ID);
-    (*buf)+=sizeof(field);
+    deserializer.End();
 }
+
+void ParseCreateTable(uint8_t* buf) {
+
+}
+
+void ParseWalRecord(duckdb::BinaryDeserializer& deserializer) {
+    auto wal_enum_type = deserializer.ReadProperty<duckdb::WALType>(100, "wal_type");
+
+
+    switch (wal_enum_type) {
+    case duckdb::WALType::INVALID:
+        throw std::runtime_error{"invalid wal type"};
+        break;
+    case duckdb::WALType::CREATE_TABLE:
+    
+    break;
+    case duckdb::WALType::DROP_TABLE:
+    
+    break;
+    case duckdb::WALType::CREATE_SCHEMA:
+    
+    break;
+    case duckdb::WALType::DROP_SCHEMA:
+    
+    break;
+    case duckdb::WALType::CREATE_VIEW:
+    
+    break;
+    case duckdb::WALType::DROP_VIEW:
+    
+    break;
+    case duckdb::WALType::CREATE_SEQUENCE:
+    
+    break;
+    case duckdb::WALType::DROP_SEQUENCE:
+    
+    break;
+    case duckdb::WALType::SEQUENCE_VALUE:
+    
+    break;
+    case duckdb::WALType::CREATE_MACRO:
+    
+    break;
+    case duckdb::WALType::DROP_MACRO:
+    
+    break;
+    case duckdb::WALType::CREATE_TYPE:
+    
+    break;
+    case duckdb::WALType::DROP_TYPE:
+    
+    break;
+    case duckdb::WALType::ALTER_INFO:
+    
+    break;
+    case duckdb::WALType::CREATE_TABLE_MACRO:
+    
+    break;
+    case duckdb::WALType::DROP_TABLE_MACRO:
+    
+    break;
+    case duckdb::WALType::CREATE_INDEX:
+    
+    break;
+    case duckdb::WALType::DROP_INDEX:
+    
+    break;
+    case duckdb::WALType::USE_TABLE:
+    
+    break;
+    case duckdb::WALType::INSERT_TUPLE:
+    
+    break;
+    case duckdb::WALType::DELETE_TUPLE:
+    
+    break;
+    case duckdb::WALType::UPDATE_TUPLE:
+    
+    break;
+    case duckdb::WALType::ROW_GROUP_DATA:
+    
+    break;
+    case duckdb::WALType::WAL_VERSION:
+    
+    break;
+    case duckdb::WALType::CHECKPOINT:
+    
+    break;
+    case duckdb::WALType::WAL_FLUSH:
+    
+    break;
+    }
+}
+
+class BufferStream : public duckdb::ReadStream {
+    uint8_t* buffer_;
+    uint8_t* end_ptr_;
+public:
+
+    BufferStream(uint8_t* buffer, size_t sz) : buffer_(buffer), end_ptr_(buffer_ + sz) {}
+    void ReadData(uint8_t* dst, idx_t read_size) override {
+        memcpy(dst, buffer_, read_size);
+        buffer_ += read_size;
+    }
+
+    bool Done() {
+        return buffer_ == end_ptr_;
+    }
+
+    uint8_t* GetBuffer() {
+        return buffer_;
+    }
+
+    size_t GetRemainder() {
+        return end_ptr_ - buffer_;
+    }
+};
 
 void ScanWal(uint8_t* buf, uint64_t sz) {
-    uint8_t* end = buf + sz;
-    uint8_t* start = buf;
-    ReadVersion(&buf);
-    ASSERT(buf - start == 8);
 
-    while(buf < end) {
-        uint64_t log_record_size = *(uint64_t*)buf;
-        buf += 8;
-        uint64_t log_record_checksum = *(uint64_t*)buf;
-        buf += 8;
-        ASSERT((end - buf) >= log_record_size);
+    BufferStream stream(buf, sz);
+    duckdb::BinaryDeserializer deserializer(stream);
+
+    ReadVersion(deserializer);
+    ASSERT(sz - stream.GetRemainder() == 8);
+
+    while(!stream.Done()) {
+        uint64_t log_record_size;
+        uint64_t log_record_checksum;
+        stream.ReadData((uint8_t*)&log_record_size, sizeof(log_record_size));
+        stream.ReadData((uint8_t*)&log_record_checksum, sizeof(log_record_checksum));
+        ASSERT(stream.GetRemainder() >= log_record_size);
 
         // parameter should be const here probably.
         // if it does write into it parameter, it will crash because of PROT_READ.
-        uint64_t checksum = duckdb::Checksum(buf, log_record_size);
+        std::unique_ptr<uint8_t[]> log_record_buf(new uint8_t[log_record_size]);
+        stream.ReadData(log_record_buf.get(), log_record_size);
+        uint64_t checksum = duckdb::Checksum(log_record_buf.get(), log_record_size);
         ASSERT(checksum == log_record_checksum);
-        buf += log_record_size;
+
+        //ParseWalRecord(buf, log_record_size);
     }
-    ASSERT(buf == end);
 }
 
 int main(int argc, char** argv) {
     std::string wal_path = argv[1];
     auto wal_buffer = OpenWal(wal_path);
-    ScanWal(wal_buffer.buf.get(), wal_buffer.size);
+    ScanWal(wal_buffer.first.get(), wal_buffer.second);
     return 0;
 }
 
